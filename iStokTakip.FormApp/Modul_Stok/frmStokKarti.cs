c8749062faa7547273c7c516b92fae09692ce973
Batuhan Kadik <batuhankadik@outlook.com>
2018-01-23 12:04:25 +0300

    first

---
+++ iStokTakip.FormApp/Modul_Stok/frmStokKarti.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace iStokTakip.FormApp.Modul_Stok
+{
+    public partial class frmStokKarti : DevExpress.XtraEditors.XtraForm
+    {
+        public frmStokKarti()
+        {
+            InitializeComponent();
+        }
+
+        private void frmStokKarti_Load(object sender, EventArgs e)
+        {
+            Listele();
+        }
+        void Listele()
+        {
+
+        }
+
+       
+    }
+}
